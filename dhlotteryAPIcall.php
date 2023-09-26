faf62d59d7e1b29808e1fd3b17a3e39d802923ca
ckdmin <57394968+ckdmin@users.noreply.github.com>
2023-09-26 13:45:09 +0900

    Update dhlotteryAPIcall.php

--- dhlotteryAPIcall.php
+++ dhlotteryAPIcall.php
@@ -8,7 +8,7 @@ $ch = curl_init();                                 //curl 초기화
 curl_setopt($ch, CURLOPT_URL, $url);
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
- 
+
 $response = curl_exec($ch);
 curl_close($ch);
 
