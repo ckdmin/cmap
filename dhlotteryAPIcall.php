8dc5b3f67bdda020b952f43deb153b7349c358f5
ckdmin <57394968+ckdmin@users.noreply.github.com>
2023-09-26 13:13:22 +0900

    Update dhlotteryAPIcall.php

--- dhlotteryAPIcall.php
+++ dhlotteryAPIcall.php
@@ -2,7 +2,7 @@
 
 $url = "https://dhlottery.co.kr/store.do?method=sellerInfo645Result&searchType=3&sltSIDO2=%EA%B2%BD%EA%B8%B0&sltGUGUN2&rtlrSttus=001&nowPage=1";
 
-
+ 
 $ch = curl_init();                                 //curl 초기화
 
 curl_setopt($ch, CURLOPT_URL, $url);
