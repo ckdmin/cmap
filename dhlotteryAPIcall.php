cfe63a983cd24b9b59bd7d2e5852233754bd9099
ckdmin <57394968+ckdmin@users.noreply.github.com>
2023-09-26 10:30:55 +0900

    ..

---
+++ dhlotteryAPIcall.php
@@ -0,0 +1,18 @@
+<?php
+
+$url = "https://dhlottery.co.kr/store.do?method=sellerInfo645Result&searchType=3&sltSIDO2=%EA%B2%BD%EA%B8%B0&sltGUGUN2&rtlrSttus=001&nowPage=".$_GET["PAGENO"];
+
+$ch = curl_init();                                 //curl 초기화
+ 
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+ 
+$response = curl_exec($ch);
+curl_close($ch);
+
+ echo $response;
+
+// return $response;
+
+?>
