7729750e16be01ad3613f41e402bd5ff1cccd34d
ckdmin <57394968+ckdmin@users.noreply.github.com>
2023-09-26 10:52:54 +0900

    Update dhlotteryAPIcall.php

--- dhlotteryAPIcall.php
+++ dhlotteryAPIcall.php
@@ -3,7 +3,10 @@
 $url = "https://dhlottery.co.kr/store.do?method=sellerInfo645Result&searchType=3&sltSIDO2=%EA%B2%BD%EA%B8%B0&sltGUGUN2&rtlrSttus=001&nowPage=".$_GET["PAGENO"];
 
 $ch = curl_init();                                 //curl 초기화
- 
+
+var_dump($url);
+print_r($url);
+
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
