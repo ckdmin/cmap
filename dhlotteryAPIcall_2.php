6340f74ace8ac00fc450ae5afc347664baa41a79
ckdmin <57394968+ckdmin@users.noreply.github.com>
2023-09-26 14:48:58 +0900

    ...

--- dhlotteryAPIcall_2.php
+++ dhlotteryAPIcall_2.php
@@ -1,35 +1,26 @@
 <?php
-// REST API 엔드포인트 URL
-$apiUrl = "https://dhlottery.co.kr/store.do?method=sellerInfo645Result&searchType=3&sltSIDO2=%EA%B2%BD%EA%B8%B0&sltGUGUN2&rtlrSttus=001&nowPage=1";
 
-// CURL 세션 초기화
-$ch = curl_init($apiUrl);
+//$url = "https://dapi.kakao.com/route-affiliate/v2/car.json?x=127.91903257369995,127.92853832244874&y=37.32480788945914,37.31989347606299&routeMode=SHORTEST_REALTIME&routeOption=NONE&inputCoordSystem=WGS84&outputCoordSystem=WGS84";
+//$headers = array(
+//	"Authorization: KakaoAK 21dbcfabd7944c87f6fbd8390b175389"
+//);
 
-// CURL 옵션 설정
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 결과를 문자열로 반환
-// 필요한 경우 다른 CURL 옵션 설정
-// 예를 들어 POST 요청을 하려면 아래 옵션을 추가할 수 있습니다.
-// curl_setopt($ch, CURLOPT_POST, true); // POST 요청 설정
-// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData)); // 요청 본문 설정 (JSON 데이터)
+$url = "https://dhlottery.co.kr/store.do?method=sellerInfo645Result&searchType=3&sltSIDO2=%EA%B2%BD%EA%B8%B0&sltGUGUN2&rtlrSttus=001&nowPage=1";
 
-// 필요한 경우 인증 헤더를 추가할 수 있습니다.
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//     'Authorization: Bearer YOUR_ACCESS_TOKEN',
-//     'Content-Type: application/json',
-//     // 다른 헤더 추가 가능
-// ));
 
-// CURL 요청 실행
+$ch = curl_init();                                 //curl 초기화
+ 
+curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_HEADER, true);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+ 
 $response = curl_exec($ch);
-
-// CURL 세션 종료
 curl_close($ch);
 
-if ($response === false) {
-    // CURL 요청 실패 시 에러 처리
-    echo "CURL 요청 실패: " . curl_error($ch);
-} else {
-    // CURL 요청 성공 시 응답 출력 또는 처리
-    echo "API 응답: " . $response;
-}
+ echo $response;
+
+// return $response;
+
 ?>
